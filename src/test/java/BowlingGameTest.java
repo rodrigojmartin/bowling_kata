4c34cdffb7b83ed662d66af5511e5a8f13846092
Rodrigo Julian Martin <rjulianmartin@gmail.com>
2018-06-01 23:07:26 -0300

    Refactor: Add setup for Game initialization

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -6,9 +7,15 @@ import static org.junit.Assert.assertEquals;
  * Created by Rodrigo on 6/1/18.
  */
 public class BowlingGameTest {
+    private Game g;
+
+    @Before
+    public void setUp() {
+        g = new Game();
+    }
+
     @Test
     public void testGutterGame() {
-        Game g = new Game();
         for (int i=0; i<20; i++) {
             g.roll(0);
         }
@@ -18,7 +25,6 @@ public class BowlingGameTest {
 
     @Test
     public void testAllOnes() {
-        Game g = new Game();
         for (int i=0; i<20; i++) {
             g.roll(1);
         }
