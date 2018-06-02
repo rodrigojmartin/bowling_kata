2e16dfe5b21b6a33bb408a8af9cede914b9cc0ec
Rodrigo Julian Martin <rjulianmartin@gmail.com>
2018-06-01 23:02:56 -0300

    The second test, all ones with minimal implementation.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,10 +2,13 @@
  * Created by Rodrigo on 6/1/18.
  */
 public class Game {
+    private int score;
+
     public void roll(int pins) {
+        score += pins;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -10,9 +10,19 @@ public class BowlingGameTest {
     public void testGutterGame() {
         Game g = new Game();
         for (int i=0; i<20; i++) {
-            g.roll(20);
+            g.roll(0);
         }
         assertEquals(0 ,g.score());
+
+    }
+
+    @Test
+    public void testAllOnes() {
+        Game g = new Game();
+        for (int i=0; i<20; i++) {
+            g.roll(1);
+        }
+        assertEquals(20 ,g.score());
 
     }
 }
